53f7109cbdb982339dff9394dbbcb3fd5ba24d1a
Park Min Hyun <minhyun102947@gmail.com>
2025-04-15 11:47:13 +0900

    [Bronze III] Title: 알람 시계, Time: 200 ms, Memory: 18480 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+      Scanner sc =new Scanner(System.in);
+      int a =sc.nextInt();
+      int b =sc.nextInt();
+      b-=45;
+      if(b<0){
+        a--;
+        b+=60;
+        if(a<0){
+          a=23;
+        }
+      }
+      System.out.println(a+" "+b);
+  }
+}
