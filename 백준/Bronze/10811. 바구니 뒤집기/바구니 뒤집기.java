edbd898a07112086734ebca65696a2e369ece68f
Park Min Hyun <minhyun102947@gmail.com>
2025-04-15 17:47:00 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 204 ms, Memory: 18540 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,37 @@
+
+
+import java.util.Scanner;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+
+        int[] arr=new int[a];
+
+        for(int i=0;i<a;i++){
+          arr[i]=i+1;
+        }
+
+        for(int i=0;i<b;i++){
+          int c=sc.nextInt()-1;
+          int d=sc.nextInt()-1;
+          while(c<d){
+            int tmp=arr[c];
+            arr[c]=arr[d];
+            arr[d]=tmp;
+            c++;
+            d--;
+          }
+        }
+        for(int num:arr){
+          System.out.print(num+" ");
+        }
+
+    }   
+}
+
