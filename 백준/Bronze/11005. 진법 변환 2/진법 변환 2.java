dbf317bce4581f60ba8f4e770814d367c65fe36d
Park Min Hyun <minhyun102947@gmail.com>
2025-04-17 15:20:13 +0900

    [Bronze I] Title: 진법 변환 2, Time: 180 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) { 
+      Scanner sc =new Scanner(System.in);
+      int N = sc.nextInt();
+      int B = sc.nextInt();
+
+      StringBuilder sb =new StringBuilder();
+      while(N>0){
+        int num = N%B;
+        if(num<10){
+          sb.append((char)(num+'0'));
+        }else{
+          sb.append((char)(num-10+'A'));
+        }
+        N/=B;
+      }
+      System.out.println(sb.reverse().toString());
+    }
+}
+
