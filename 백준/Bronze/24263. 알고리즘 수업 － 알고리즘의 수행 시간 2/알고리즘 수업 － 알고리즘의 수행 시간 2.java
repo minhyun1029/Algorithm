a3e2457b9c70088dc9509de487f5ef0010d94e8c
Park Min Hyun <minhyun102947@gmail.com>
2025-04-21 10:04:43 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 172 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num =sc.nextInt();
+        System.out.println(num);
+        System.out.println(1);
+            
+        
+    }
+}
